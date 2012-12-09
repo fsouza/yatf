bd32d6ed3836869bbc65b90aece4f4152ec5aee1
Francisco Souza <f@souza.cc>
2012-12-09 21:00:30 -0200

    yactf: added first version of yactf

---
+++ yactf.c
@@ -0,0 +1,94 @@
+// Copyright 2012 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "yactf.h"
+
+static registry_t *r = NULL;
+
+void
+_register_test(char *name, void (*fn)(test_t *t))
+{
+	test_t *t;
+	t = (test_t *)malloc(sizeof(test_t));
+	if(t == NULL) {
+		fprintf(stderr, "Out of memory, can't allocate memory for the test.\n");
+	}
+	t->name = name;
+	t->failed = 0;
+	t->failures = NULL;
+	t->fn = fn;
+	registry_t *p = (registry_t *)malloc(sizeof(registry_t));
+	p->item = t;
+	p->next = r;
+	r = p;
+}
+
+void
+free_test(test_t *t)
+{
+	failure_t *f;
+	while(t->failures != NULL) {
+		f = t->failures;
+		t->failures = t->failures->next;
+		free(f);
+	}
+}
+
+void
+free_suite()
+{
+	registry_t *p;
+	while(r != NULL) {
+		p = r;
+		r = r->next;
+		free_test(p->item);
+		free(p->item);
+		free(p);
+	}
+}
+
+void
+test_fail(test_t *t, char* reason)
+{
+	t->failed = 1;
+	failure_t *failure;
+	failure = (failure_t *)malloc(sizeof(failure_t));
+	failure->reason = reason;
+	failure->next = t->failures;
+	t->failures = failure;
+}
+
+int
+run_tests()
+{
+	int ran, failed, plural;
+	ran = failed = 0;
+	if(r == NULL) {
+		printf("Warning: no tests to run.\n");
+		return 0;
+	}
+	registry_t *p = r;
+	while(p != NULL) {
+		p->item->fn(p->item);
+		ran++;
+		if(p->item->failed) {
+			printf("FAIL: %s\n", p->item->name);
+			failure_t *f;
+			f = p->item->failures;
+			while(f != NULL) {
+				printf("      %s\n", f->reason);
+				f = f->next;
+			}
+			printf("\n");
+			failed++;
+		}
+		p = p->next;
+	}
+	free_suite();
+	plural = ran > 1 ? 's' : '\0';
+	printf("Ran %d test%c. Failures: %d.\n", ran, plural, failed);
+	return failed > 0;
+}
---
+++ yactf.h
@@ -0,0 +1,36 @@
+// Copyright 2012 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#define register_test(fn) _register_test(#fn, fn)
+#define test_fatal(t, reason) test_fail(t, reason); return
+
+struct failure_t
+{
+	char             *reason;
+	struct failure_t *next;
+};
+
+struct test_t
+{
+	char             *name;
+	struct failure_t *failures;
+	int              failed;
+	void (*fn)(struct test_t *t);
+};
+
+struct test_list_t
+{
+	struct test_t      *item;
+	struct test_list_t *next;
+};
+
+typedef struct failure_t failure_t;
+typedef struct test_t test_t;
+typedef struct test_list_t registry_t;
+
+void test_fail(test_t *t, char *reason);
+void _register_test(char *name, void (*fn)(test_t *t));
+void free_test(test_t *t);
+void free_registry();
+int run_tests();
