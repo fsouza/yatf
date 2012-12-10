9ea9f56bfcab4813998540ed0e92f7156497e946
Francisco Souza <f@souza.cc>
2012-12-09 22:05:41 -0200

    tests: added three initial tests
    More tests are coming in near future :-)

---
+++ tests/test_multiple.c
@@ -0,0 +1,51 @@
+// Copyright 2012 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Output: Ran 3 tests. Failures: 0.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <yactf.h>
+
+int
+sum(int x, int y)
+{
+	return x + y;
+}
+
+void
+test_sum_1_and_3(test_t *t)
+{
+	int got = sum(1, 3);
+	if(got != 4) {
+		test_fail(t, "1 + 3 should be 4!");
+	}
+}
+
+void
+test_sum_1_and_4(test_t *t)
+{
+	int got = sum(1, 4);
+	if(got != 5) {
+		test_fail(t, "1 + 4 should be 5!");
+	}
+}
+
+void
+test_sum_2_and_3(test_t *t)
+{
+	int got = sum(2, 3);
+	if(got != 5) {
+		test_fail(t, "2 + 3 should be 5!");
+	}
+}
+
+int
+main(void)
+{
+	register_test(test_sum_1_and_3);
+	register_test(test_sum_1_and_4);
+	register_test(test_sum_2_and_3);
+	return run_tests();
+}
---
+++ tests/test_one_test.c
@@ -0,0 +1,48 @@
+// Copyright 2012 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Output: Ran 1 test. Failures: 0.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <yactf.h>
+
+int
+sum(int x, int y)
+{
+	return x + y;
+}
+
+void
+test_sum(test_t *t)
+{
+	int i;
+	static struct
+	{
+		int x, y;
+		int expected;
+	} input[] = {
+		{2, 4, 6},
+		{2, 3, 5},
+		{50, -1, 49},
+		{20, 32, 52},
+		{0, 0, 0},
+	};
+	for(i = 0; input[i].x != 0; i++) {
+		int got = sum(input[i].x, input[i].y);
+		if(got != input[i].expected) {
+			char *reason = (char *)malloc(100 * sizeof(char));
+			sprintf(reason, "Fail: sum(%d, %d). Want %d. Got %d.",
+			          input[i].x, input[i].y, input[i].expected, got);
+			test_fail(t, reason);
+		}
+	}
+}
+
+int
+main(void)
+{
+	register_test(test_sum);
+	return run_tests();
+}
